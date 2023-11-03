db8479aedd291ceefbc4911093900d39bf3099f5
Eileen Zhang <eileen.wei.zhang@gmail.com>
2023-11-03 09:37:13 -0400

    Momentum V2

--- data/sharadar_prices.py
+++ data/sharadar_prices.py
@@ -2,7 +2,7 @@ import nasdaqdatalink as quandl
 import os
 import pandas as pd
 from data import investment_universe
-from datetime import date
+from datetime import date, timedelta
 from common import date_util
 
 
@@ -36,8 +36,23 @@ def get_prices(tickers, as_of_date, exit_date=None, refresh=False, update=False)
     return price_df
 
 
+def get_90d_vol(tickers, as_of_date):
+    path = "/Users/weizhang/Documents/_GIT/quant-strategies/data/Signals/Vol_90D/signal_" + as_of_date.strftime("%Y-%m-%d") + ".csv"
+    if os.path.isfile(path):
+        existing_signal_df = pd.read_csv(path)
+        existing_signal_df.set_index("ticker", inplace=True)
+        return existing_signal_df
+    price_df = quandl.get_table('SHARADAR/SEP', date={'gte':(as_of_date - timedelta(weeks=13)).strftime("%Y-%m-%d"), 'lte':as_of_date.strftime("%Y-%m-%d")}, ticker=tickers, paginate=True)
+    signal_df = pd.DataFrame(price_df.pivot(index='date', columns='ticker', values='closeadj').pct_change().tail(-1).std(axis=0))
+    signal_df.columns = ["Vol_90D"]
+    signal_df.to_csv(path)
+    return signal_df
+
+
 if __name__ == "__main__":
     quandl.ApiConfig.api_key = 'NRvcyMwNMXZ2ooDSM3nw'
-    universe = investment_universe.get_SPX(date(2001, 9, 5))
-    prices = get_prices(universe['ticker'].to_list(), date(2001, 9, 5), date(2001, 9, 13), refresh=True)
-    print(prices)
+    universe = investment_universe.get_SPX(date(2023, 9, 30))
+    signal_df = get_90d_vol(universe['ticker'].to_list(), date(2023, 9, 30))
+    # prices = get_prices(universe['ticker'].to_list(), date(2001, 9, 5), date(2001, 9, 13), refresh=True)
+    # print(prices)
+    print("Hello World")
